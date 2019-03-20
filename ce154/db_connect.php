206ff3e60349a0910666e4d3d3eac24a9eea01f2
bu18777 <45286662+bu18777@users.noreply.github.com>
2019-03-20 00:56:30 +0000

    Update db_connect.php

--- ce154/db_connect.php
+++ ce154/db_connect.php
@@ -1,7 +1,7 @@
 <?php
 $DB_HOST = 'cseemyweb.essex.ac.uk';
 $DB_USER = 'bu18777';
-$DB_PASS = '4jcDj9ZbwUT0f';
+$DB_PASS = '';
 $DB_NAME = 'ce154_bu18777';
 
 $link = mysqli_connect($DB_HOST,$DB_USER,$DB_PASS,$DB_NAME);
